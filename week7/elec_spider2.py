e05880ba79f61c2715b506a1289f0001d84ae882
zoe <zuozuodifei@126.com>
2021-01-06 16:26:07 +0800

    国家电网1.0版本数据处理

--- week7/elec_spider2.py
+++ week7/elec_spider2.py
@@ -5,6 +5,7 @@ import urllib
 import os
 import zipfile
 import shutil
+import pandas as pd
 
 def spider(begin):
     try:
@@ -66,25 +67,23 @@ def spider(begin):
 ## 解压文件
 def unzipfile():
     filepath = './unpacked'
-
     ## 如果解压文件夹不存在就创建，如果存在就先清空再创建
+   #shutil.rmtree(filepath)
     if not os.path.exists(filepath):
         os.mkdir(filepath)
     else:
         shutil.rmtree(filepath)
         os.mkdir(filepath)
-
     failnum = 0
     sucessnum = 0
     for zipfiles in os.listdir('./savefiles'):
         if not zipfiles.startswith('.'):  # 排除隐藏文件
-
             filedirectory = re.search('(.*?).zip', zipfiles).group(1)
             print(filedirectory)
             filecontents = zipfile.ZipFile('./savefiles/%s' % zipfiles, 'r')
             for file in filecontents.namelist():
-                if not os.path.isfile(file):
-                    print(file)
+                # if not os.path.isfile(file):
+                #     print(file)
                 try:
                     print(file)
                     newname = file.encode('cp437').decode('gbk')
@@ -102,9 +101,68 @@ def unzipfile():
     print("失败解压文件数：%d"%failnum)
     print("成功解压文件数：%d"%sucessnum)
 
+# 写入状态
+def write_status():
+    listdir = []
+    for hide in os.listdir('./unpacked'):
+        if not hide.startswith('.'):
+            listdir.append(hide)
+    print(listdir)
+    for char in listdir:
+        status = re.match('(\w){4}',char).group()
+        print(status)
+        writein = open('./unpacked/%s/status.txt'%char,'w')
+        writein.write(status)
+        writein.close()
+
+# 将状态写入货物清单
+def write_list():
+    filepath = './unpacked'
+    listdir = []
+    dataframe = []
+    for hide in os.listdir(filepath):
+        if not hide.startswith('.'):
+            listdir.append(hide)
+    for i in listdir:
+        i = os.path.join(filepath,i)
+        for j in os.listdir(i):
+            if re.search('[货物清单].*?.xls',j):
+                if not j.startswith('.'):
+                    excel = os.path.join(i,j)
+                    #df = pd.read_excel(excel)
+                    sheetall = pd.ExcelFile(excel)
+                    status = open('%s/status.txt'%i,'r')
+                    sta = status.read()
+                    for sheet in sheetall.sheet_names:
+                        colnum = 0  # 计量表头含有空列的数量
+                        # 判断表头是否含有unamed 的空列
+                        for col in list(sheetall.parse(sheet_name=sheet).columns):
+                            if 'Unnamed' in col :
+                                colnum +=1
+                        #print(colnum)
+                        # 如果含有Unamed的空列数量 大于3 就略过第一行，取第二行作为表头，否则取第一行作为表头
+                        if  colnum >3 :
+                            df = sheetall.parse(sheet_name=sheet,skiprows=1)
+                        else:
+                            df = sheetall.parse(sheet_name=sheet)
+                        #print(df.columns)
+                        df['项目状态'] = sta
+                        dataframe.append(df)
+    dataall = pd.concat(dataframe)
+    print(len(dataall.columns))
+    col = ['包号','网省采购申请行号','项目单位','需求单位','项目名称','工程电压等级','物资名称','物资描述','单位','数量','交货日期','交货地点','备注','技术规范ID','项目状态']
+    df3 = dataall[col]
+    print(df3.info())
+
+
+
+
 if __name__ == "__main__":
     # print("准备爬取电网招标文书。")
     # start = input("请输入爬取起始页码：")
     # spider(int(start))
-    unzipfile()
+    #unzipfile()
+    #write_status()
+    write_list()
+
 
